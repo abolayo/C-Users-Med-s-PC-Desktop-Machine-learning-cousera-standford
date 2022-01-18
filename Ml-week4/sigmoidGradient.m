ab9fb480830e7f39bf66e675d933ddbba1eb52eb
abolayo <medinatapanpa@gmail.com>
2022-01-18 12:19:14 +0100

    exercise completed

--- Ml-week4/sigmoidGradient.m
+++ Ml-week4/sigmoidGradient.m
@@ -12,9 +12,8 @@ g = zeros(size(z));
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
-
-
-
+sigz = sigmoid(z);
+g = sigz .* (1-sigz);
 
 
 
